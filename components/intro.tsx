4a7984b06ee1287657515695bb4bcf614493b8f4
Desmond Ofori <oforidesmond@rocketmail.com>
2024-02-02 14:08:43 +0000

    change picture format

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import myPic from "../../portfolio-website/public/my-pic.jpg";
+import myPic from "@/public/my-pic.png";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
